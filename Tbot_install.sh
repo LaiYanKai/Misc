9b0d78149f355a2349003a679f2b13c5c24c21d4
Lai Yan Kai <lai.yankai@gmail.com>
2021-01-31 15:00:43 +0800

    Add files via upload

--- Tbot_install.sh
+++ Tbot_install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 sudo wget -O /etc/apt/apt.conf.d/20auto-upgrades https://raw.githubusercontent.com/LaiYanKai/EE4308/main/20auto-upgrades
 #Configure the Raspberry Pi
 systemctl mask systemd-networkd-wait-online.service
