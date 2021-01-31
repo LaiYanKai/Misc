6a369b626ae5bf443bfbb150a111831cf76d3687
Lai Yan Kai <lai.yankai@gmail.com>
2021-01-31 18:17:31 +0800

    Add files via upload

--- Tbot_install2.sh
+++ Tbot_install2.sh
@@ -4,8 +4,8 @@ sudo apt-get upgrade --yes
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install ros-melodic-ros-base
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install ros-melodic-ros-base --yes
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential --yes
 sudo rosdep init
 rosdep update
 #Install and build TurtleBot3 packages. 
