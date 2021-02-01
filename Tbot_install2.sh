64362e5a955ae77cda24167128b55e6d07cbaee1
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-01 20:23:51 +0800

    Add files via upload

--- Tbot_install2.sh
+++ Tbot_install2.sh
@@ -6,6 +6,7 @@ sudo apt upgrade --yes
 # Install ROS Melodic Morenia
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
 sudo apt install ros-melodic-ros-base --yes
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
