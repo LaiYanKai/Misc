a477b32e9a1c66917e5e3c675e12270663bb63fe
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-01 20:26:46 +0800

    Add files via upload

--- Tbot_install2.sh
+++ Tbot_install2.sh
@@ -8,7 +8,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install ros-melodic-ros-base --yes
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential --yes
 sudo rosdep init
 rosdep update
 
