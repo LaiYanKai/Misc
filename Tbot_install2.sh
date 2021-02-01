691c0680bfaccd4b39e446f32206e755e47a517a
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-01 18:58:47 +0800

    Add files via upload

--- Tbot_install2.sh
+++ Tbot_install2.sh
@@ -6,7 +6,7 @@ sudo apt upgrade --yes
 # Install ROS Melodic Morenia
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt install ros-melodic-ros-base
+sudo apt install ros-melodic-ros-base --yes
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
@@ -30,6 +30,7 @@ echo 'source /opt/ros/melodic/setup.bash' >> ~/.bashrc
 source ~/.bashrc
 cd ~/catkin_ws && catkin_make -j1
 echo 'source ~/catkin_ws/devel/setup.bash' >> ~/.bashrc
+echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
 source ~/.bashrc
 cd ~
 
