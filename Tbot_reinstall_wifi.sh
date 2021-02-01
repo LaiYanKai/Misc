a98b680f1a16f6830f9ff5c3bb49e4f1091a9d1d
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-01 14:42:50 +0800

    Add files via upload

--- Tbot_reinstall_wifi.sh
+++ Tbot_reinstall_wifi.sh
@@ -1,9 +1,10 @@
 #reinstall wifi firmware
 #https://www.linuxquestions.org/questions/slackware-arm-108/raspberry-pi-3-b-wifi-nic-not-found-4175627137/
 sudo apt install wireless-tools --yes
+mkdir -p ~/Downloads/rpi3_wifi_firmware
 cd ~/Downloads/rpi3_wifi_firmware
 rm -rf *
-wget  https://archive.raspberrypi.org/debian/pool/main/f/firmware-nonfree/firmware-brcm80211_20161130-3+rpt3_all.deb
+wget -O "firmware-brcm80211_20161130-3+rpt3_all.deb" "https://archive.raspberrypi.org/debian/pool/main/f/firmware-nonfree/firmware-brcm80211_20161130-3+rpt3_all.deb"
 ar x firmware-brcm80211_20161130-3+rpt3_all.deb
 tar xvf data.tar.xz 
 cd /lib/firmware/brcm/
