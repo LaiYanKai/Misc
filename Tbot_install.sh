93c04110c7d72b7a9ccdb78928e79172ce64d56d
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-01 11:39:10 +0800

    Add files via upload

--- Tbot_install.sh
+++ Tbot_install.sh
@@ -1,4 +1,5 @@
 #Configure the Raspberry Pi
 systemctl mask systemd-networkd-wait-online.service
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+sudo wget -O /etc/apt/apt.conf.d/20auto-upgrades https://raw.githubusercontent.com/LaiYanKai/EE4308/main/20auto-upgrades
 sudo reboot
