fe44312d6a15f64fbffba56cb41b929bb72d8c12
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-01 13:14:16 +0800

    Add files via upload

--- Tbot_install3.sh
+++ Tbot_install3.sh
@@ -1,7 +1,3 @@
-sudo apt-get update
-sudo apt-get upgrade --yes
-sudo wget -O /etc/apt/apt.conf.d/20auto-upgrades https://raw.githubusercontent.com/LaiYanKai/EE4308/main/20auto-upgrades
-sudo reboot
 #Install ROS Melodic Morenia
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
