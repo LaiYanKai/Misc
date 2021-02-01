b2aa5600e0a291fddf2f2897455360885089ed85
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-01 18:08:57 +0800

    Add files via upload

--- Tbot_install.sh
+++ Tbot_install.sh
@@ -1,7 +1,23 @@
 #Configure the Raspberry Pi
-sudo wget -O /etc/apt/apt.conf.d/20auto-upgrades https://raw.githubusercontent.com/LaiYanKai/EE4308/main/20auto-upgrades
+# 20auto-upgrades
+sudo sh -c "echo 'APT::Periodic::Update-Package-Lists "0";' > /etc/apt/apt.conf.d/20auto-upgrades"
+sudo sh -c "echo 'APT::Periodic::Unattended-Upgrade "0";' >> /etc/apt/apt.conf.d/20auto-upgrades"
+
+# netplan
+sudo sh -c "'network:' > /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'  version: 2' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'  ethernets:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'    eth0:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'      dhcp4: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'      optional: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'  wifis:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'    wlan0:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'      dhcp4: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'      optional: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'#      access-points: ' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'#        <WIFI_SSID>: ' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "'#          password: <WIFI_PASSWORD>' >> /etc/netplan/50-cloud-init.yaml"
 sudo wget -O Tbot_reinstall_wifi.sh https://raw.githubusercontent.com/LaiYanKai/EE4308/main/Tbot_reinstall_wifi.sh
-sudo wget -O Tbot_install3.sh https://raw.githubusercontent.com/LaiYanKai/EE4308/main/Tbot_install3.sh
 sudo wget -O Tbot_install2.sh https://raw.githubusercontent.com/LaiYanKai/EE4308/main/Tbot_install2.sh
 sudo chmod +x *.sh
 sudo reboot
--- Tbot_install2.sh
+++ Tbot_install2.sh
@@ -1,5 +1,58 @@
 systemctl mask systemd-networkd-wait-online.service
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
-sudo apt-get update
-sudo apt-get upgrade --yes
+sudo apt update
+sudo apt upgrade --yes
+
+# Install ROS Melodic Morenia
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install ros-melodic-ros-base
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo rosdep init
+rosdep update
+
+# Install ROS Melodic From ROBOTIS
+# wget https://raw.githubusercontent.com/ROBOTIS-GIT/robotis_tools/master/install_ros_melodic_rp3.sh
+# chmod 755 ./install_ros_melodic_rp3.sh
+# bash ./install_ros_melodic_rp3.sh
+
+#Install and build TurtleBot3 packages. 
+sudo apt install ros-melodic-rosserial-python ros-melodic-tf --yes
+mkdir -p ~/catkin_ws/src && cd ~/catkin_ws/src
+sudo apt install ros-melodic-hls-lfcd-lds-driver --yes
+sudo apt install ros-melodic-turtlebot3-msgs --yes
+sudo apt install ros-melodic-dynamixel-sdk --yes
+git clone -b melodic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+cd ~/catkin_ws/src/turtlebot3
+rm -r turtlebot3_description/ turtlebot3_teleop/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
+cd ~/catkin_ws/
+echo 'source /opt/ros/melodic/setup.bash' >> ~/.bashrc
+source ~/.bashrc
+cd ~/catkin_ws && catkin_make -j1
+echo 'source ~/catkin_ws/devel/setup.bash' >> ~/.bashrc
+source ~/.bashrc
+cd ~
+
+# USB Port Setting
+rosrun turtlebot3_bringup create_udev_rules
+
+# OpenCR
+sudo dpkg --add-architecture armhf
+sudo apt update
+sudo apt install libc6:armhf --yes
+
+export OPENCR_PORT=/dev/ttyACM0
+export OPENCR_MODEL=burger
+rm -rf ./opencr_update.tar.bz2
+
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS1/latest/opencr_update.tar.bz2 
+tar -xvf opencr_update.tar.bz2 
+
+cd ./opencr_update
+./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
+cd ~
+
+sudo apt install wireless-tools --yes
+# restore wifi
+source Tbot_reinstall_wifi.sh
 sudo reboot
