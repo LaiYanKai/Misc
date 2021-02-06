a9043ab3ad091c40ada2b54e4c8e3bb8e05f53bd
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-06 09:52:28 +0800

    Add files via upload

---
+++ rubiker.sh
@@ -0,0 +1,20 @@
+# wget raw.githubusercontent.com/LaiYanKai/EE4308/main/rubiker.sh
+sudo sh -c "echo 'APT::Periodic::Update-Package-Lists "0";' > /etc/apt/apt.conf.d/20auto-upgrades"
+sudo sh -c "echo 'APT::Periodic::Unattended-Upgrade "0";' >> /etc/apt/apt.conf.d/20auto-upgrades"
+
+# netplan
+sudo sh -c "echo 'network:' > /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '  version: 2' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '  ethernets:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '    eth0:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '      dhcp4: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '      optional: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '#  wifis:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '#    wlan0:' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '#      dhcp4: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '#      optional: true' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '#      access-points: ' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '#        <WIFI_SSID>: ' >> /etc/netplan/50-cloud-init.yaml"
+sudo sh -c "echo '#          password: <WIFI_PASSWORD>' >> /etc/netplan/50-cloud-init.yaml"
+wget raw.githubusercontent.com/LaiYanKai/EE4308/main/rubiker2.sh
+sudo reboot
---
+++ rubiker2.sh
@@ -0,0 +1,16 @@
+# source rubiker2.sh
+systemctl mask systemd-networkd-wait-online.service
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+sudo apt update
+sudo apt upgrade --yes
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install ros-noetic-ros-base --yes
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential --yes
+sudo apt install python3-rosdep --yes
+sudo rosdep init
+rosdep update
