0285ce7e5fb074b1f611254fa6e05312962a25cf
Lai Yan Kai <lai.yankai@gmail.com>
2021-02-09 11:54:49 +0800

    Update install_TPLinkAC1300T3U.sh

--- install_TPLinkAC1300T3U.sh
+++ install_TPLinkAC1300T3U.sh
@@ -1,14 +1,14 @@
 # github.com/RinCat/RTL88x2BU-Linux-Driver
 
 # Manual DKMS installation
-git clone "https://github.com/RinCat/RTL88x2BU-Linux-Driver.git" /usr/src/rtl88x2bu-git
+sudo git clone "https://github.com/RinCat/RTL88x2BU-Linux-Driver.git" /usr/src/rtl88x2bu-git
 #sed -i 's/PACKAGE_VERSION="@PKGVER@"/PACKAGE_VERSION="git"/g' /usr/src/rtl88x2bu-git/dkms.conf
 #dkms add -m rtl88x2bu -v git
 #dkms autoinstall
 #modprobe 88x2bu
 
 #git clone https://github.com/cilynx/rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959.git
-cd /usr/src/rtl188x2bu-git
+cd /usr/src/rtl88x2bu-git
 VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
 sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
 sudo dkms add -m rtl88x2bu -v ${VER}
