d3ca76d6bfd95b0bfd029a1b7636b26bad8e2469
Lai Yan Kai <lai.yankai@gmail.com>
2021-08-12 13:36:55 +0800

    final

--- EE3305-noetic-install.sh
+++ EE3305-noetic-install.sh
@@ -7,7 +7,7 @@ cd src
 git clone https://github.com/husky/husky
 cd ..
 catkin_make
-sudo apt install ros-noetic-robot-localization ros-noetic-interactive-marker-twist-server ros-noetic-twist-mux ros-noetic-joy ros-noetic-teleop-twist-joy --yes
+sudo apt install ros-noetic-lms1xx ros-noetic-robot-localization ros-noetic-interactive-marker-twist-server ros-noetic-twist-mux ros-noetic-joy ros-noetic-teleop-twist-joy --yes
 echo "source ~/husky/devel/setup.bash" >> ~/.bashrc
 echo "export HUSKY_GAZEBO_DESCRIPTION=$(rospack find husky_gazebo)/urdf/description.gazebo.xacro" >> ~/.bashrc
 source ~/.bashrc
