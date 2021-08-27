6fde045640a0ad17e91f39c86be5cc59e0543dd0
Lai Yan Kai <lai.yankai@gmail.com>
2021-08-27 16:09:41 +0800

    Included turtle install

--- EE3305-noetic-install.sh
+++ EE3305-noetic-install.sh
@@ -1,16 +1,19 @@
-# install husky noetic devel 210812
-cd ~
-mkdir husky
-cd husky
-mkdir src
-cd src
-git clone https://github.com/husky/husky
-cd ..
+# install husky, turtle and catkin build noetic devel 210827
+
+# install husky noetic-devel branch
+mkdir -p ~/husky/src
+cd ~/husky/src
+git clone https://github.com/husky/husky -b noetic-devel
+cd ~/husky
 catkin_make
-sudo apt install ros-noetic-lms1xx ros-noetic-robot-localization ros-noetic-interactive-marker-twist-server ros-noetic-twist-mux ros-noetic-joy ros-noetic-teleop-twist-joy --yes
+
+# husky dependencies and turtle install
+sudo apt update
+sudo apt install ros-noetic-lms1xx ros-noetic-robot-localization ros-noetic-interactive-marker-twist-server ros-noetic-twist-mux ros-noetic-joy ros-noetic-teleop-twist-joy ros-noetic-turtlebot3 ros-noetic-turtlebot3-gazebo --yes
 echo "source ~/husky/devel/setup.bash" >> ~/.bashrc
 echo "export HUSKY_GAZEBO_DESCRIPTION=$(rospack find husky_gazebo)/urdf/description.gazebo.xacro" >> ~/.bashrc
+echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 source ~/.bashrc
 
+# for catkin build
 sudo apt install python3-catkin-tools --yes
-cd ~
