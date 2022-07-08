533c47529018e2eeee01fad9485a6dfae76732cf
Lai Yan Kai <lai.yankai@gmail.com>
2022-07-08 16:27:35 +0800

    Add noetic install and updated turtle install.

--- EE3305-noetic-install.sh
+++ EE3305-noetic-install.sh
@@ -1,4 +1,15 @@
 # install husky, turtle and catkin build noetic devel 211007
+# install ros
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl --yes
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-noetic-desktop-full --yes
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential --yes # build dependencies
+# husky and turtlebot dependencies
+sudo apt install ros-noetic-velodyne-description ros-noetic-lms1xx ros-noetic-robot-localization ros-noetic-interactive-marker-twist-server ros-noetic-twist-mux ros-noetic-joy ros-noetic-teleop-twist-joy ros-noetic-turtlebot3-* --yes 
 
 # install husky noetic-devel branch
 rm -rf ~/husky
@@ -10,12 +21,8 @@ cd ~/husky
 catkin_make
 
 # husky dependencies and turtle install
-sudo apt update
-sudo apt install ros-noetic-velodyne-description ros-noetic-lms1xx ros-noetic-robot-localization ros-noetic-interactive-marker-twist-server ros-noetic-twist-mux ros-noetic-joy ros-noetic-teleop-twist-joy ros-noetic-turtlebot3 ros-noetic-turtlebot3-gazebo --yes
 echo "source ~/husky/devel/setup.bash" >> ~/.bashrc
 echo "export HUSKY_GAZEBO_DESCRIPTION=$(rospack find husky_gazebo)/urdf/description.gazebo.xacro" >> ~/.bashrc
 echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 source ~/.bashrc
 
-# for catkin build
-sudo apt install python3-catkin-tools --yes
