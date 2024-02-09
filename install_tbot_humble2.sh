88394ada3e41dc42fbdc046ed622e08f6f5e52d6
Lai Yan Kai <lai.yankai@gmail.com>
2024-02-09 19:50:56 +0800

    Update install_tbot_humble2.sh

--- install_tbot_humble2.sh
+++ install_tbot_humble2.sh
@@ -1,4 +1,17 @@
-sudo apt install python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential ros-humble-hls-lfcd-lds-driver ros-humble-turtlebot3-msgs ros-humble-dynamixel-sdk libudev-dev --yes
+# may require input at sudo apt upgrade
+sudo apt update
+
+sudo apt install software-properties-common --yes
+sudo add-apt-repository universe # press enter
+sudo apt update && sudo apt install curl --yes
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt upgrade --yes
+
+sudo apt install ros-humble-ros-base python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential ros-humble-hls-lfcd-lds-driver ros-humble-turtlebot3-msgs ros-humble-dynamixel-sdk libudev-dev --yes
+
 
 mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
 git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
@@ -34,3 +47,5 @@ tar -xvf ./opencr_update.tar.bz2
 
 cd ~/opencr_update
 ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
+
+reboot
