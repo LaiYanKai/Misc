4f3aeb88f2659cd7eeaaec1a024d78e453cad5bd
Lai Yan Kai <lai.yankai@gmail.com>
2024-02-13 17:24:48 +0800

    Update install_tbot_humble2.sh

--- install_tbot_humble2.sh
+++ install_tbot_humble2.sh
@@ -7,46 +7,22 @@ sudo apt update && sudo apt install curl --yes
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
+# download tbot stuff
+mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
+git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone -b ros2-devel https://github.com/ROBOTIS-GIT/ld08_driver.git
+cd ~
+rm -rf ./opencr_update.tar.bz2
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+
+# install packages
 sudo apt update
 sudo apt upgrade --yes
 
 sudo apt install ros-humble-ros-base python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential ros-humble-hls-lfcd-lds-driver ros-humble-turtlebot3-msgs ros-humble-dynamixel-sdk libudev-dev --yes
 
-
-mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
-git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
-git clone -b ros2-devel https://github.com/ROBOTIS-GIT/ld08_driver.git
-cd ~/turtlebot3_ws/src/turtlebot3
-rm -r turtlebot3_cartographer turtlebot3_navigation2
-cd ~/turtlebot3_ws/
-echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
-source ~/.bashrc
-colcon build --symlink-install --parallel-workers 1
-echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
-source ~/.bashrc
-
-sudo cp `ros2 pkg prefix turtlebot3_bringup`/share/turtlebot3_bringup/script/99-turtlebot3-cdc.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules
-sudo udevadm trigger
-
-echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
-echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
-echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
-
-source ~/.bashrc
-
-# OPENCR
-cd ~
 sudo dpkg --add-architecture armhf
 sudo apt update
 sudo apt install libc6:armhf --yes
-export OPENCR_PORT=/dev/ttyACM0
-export OPENCR_MODEL=burger
-rm -rf ./opencr_update.tar.bz2
-wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
-tar -xvf ./opencr_update.tar.bz2
-
-cd ~/opencr_update
-./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
 
 reboot
