fea3351d0c935d6ba752b2b527fa63c8d7473da9
Lai Yan Kai <lai.yankai@gmail.com>
2024-02-13 18:19:00 +0800

    Update install_tbot_humble2.sh

--- install_tbot_humble2.sh
+++ install_tbot_humble2.sh
@@ -1,17 +1,21 @@
 # may require input at sudo apt upgrade
 sudo apt update
-
-sudo apt install software-properties-common --yes
-sudo add-apt-repository universe # press enter
-sudo apt update && sudo apt install curl --yes
+sudo add-apt-repository universe -y # press enter
+sudo apt install software-properties-common curl --yes
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # download tbot stuff
-mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
+mkdir -p ~/turtlebot3_ws/src 
+cd ~/turtlebot3_ws/src
 git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
 git clone -b ros2-devel https://github.com/ROBOTIS-GIT/ld08_driver.git
+cd ~/turtlebot3_ws/src/turtlebot3/turtlebot3_bringup/launch/
+curl -O https://raw.githubusercontent.com/LaiYanKai/Misc/main/robot_ee4308.launch.py
+cd ~/turtlebot3_ws/src/turtlebot3/turtlebot3_bringup/param/
+curl -O https://raw.githubusercontent.com/LaiYanKai/Misc/main/install_tbot_humble3.sh
 cd ~
+curl -O https://raw.githubusercontent.com/LaiYanKai/Misc/main/run.sh
 rm -rf ./opencr_update.tar.bz2
 wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
 
