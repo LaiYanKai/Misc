65f484cc174522d26144a43ef6aafeb1090ba894
Lai Yan Kai <lai.yankai@gmail.com>
2024-02-22 22:55:19 +0800

    Update humble_install.sh

--- humble_install.sh
+++ humble_install.sh
@@ -2,18 +2,16 @@ set -e # stop immediately if got problem
 
 # update and upgrade, terminator installation
 sudo apt update && sudo apt upgrade -y
-sudo apt install terminator -y
 
 # ros installation
 locale # feedback only.
-sudo apt install software-properties-common
+sudo apt install software-properties-common terminator -y
 sudo add-apt-repository universe -y #may need to press [ENTER]
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update && sudo apt upgrade -y
-sudo apt install ros-humble-desktop-full -y
-sudo apt install ros-humble-gazebo-* ros-humble-cartographer ros-humble-cartographer-ros ros-humble-navigation2 ros-humble-nav2-bringup ros-humble-dynamixel-sdk ros-humble-turtlebot3-* git ros-dev-tools -y
+sudo apt install ros-humble-desktop-full ros-humble-gazebo-* ros-humble-cartographer ros-humble-cartographer-ros ros-humble-navigation2 ros-humble-nav2-bringup ros-humble-dynamixel-sdk ros-humble-turtlebot3-* git ros-dev-tools -y
 
 # write to .bashrc
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
