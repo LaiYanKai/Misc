6417a764042d2c3d98737cb1b4688abe4485afbb
Lai Yan Kai <lai.yankai@gmail.com>
2024-11-18 14:09:25 +0800

    Update install_tbot_humble2.sh

--- install_tbot_humble2.sh
+++ install_tbot_humble2.sh
@@ -1,33 +1,46 @@
-# may require input at sudo apt upgrade
-sudo apt update
-sudo add-apt-repository universe -y # press enter
-sudo apt install software-properties-common curl --yes
+set -euxo pipefail
+
+# Accepts one argument, which is the ROS_DOMAIN_ID. Correspond to robot number.
+echo "ROS_DOMAIN_ID is $1"
+
+# Install ROS
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt upgrade -y
 
-# download tbot stuff
-mkdir -p ~/turtlebot3_ws/src 
-cd ~/turtlebot3_ws/src
+sudo apt install ros-humble-ros-base ros-dev-tools python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential ros-humble-hls-lfcd-lds-driver ros-humble-turtlebot3-msgs ros-humble-dynamixel-sdk libudev-dev
+mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
 git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
 git clone -b ros2-devel https://github.com/ROBOTIS-GIT/ld08_driver.git
-cd ~/turtlebot3_ws/src/turtlebot3/turtlebot3_bringup/launch/
-wget -O robot_ee4308.launch.py https://raw.githubusercontent.com/LaiYanKai/Misc/main/robot_ee4308.launch.py
-cd ~/turtlebot3_ws/src/turtlebot3/turtlebot3_bringup/param/
-wget -O burger_ee4308.yaml https://raw.githubusercontent.com/LaiYanKai/Misc/main/burger_ee4308.yaml
-cd ~
-wget -O robot.sh https://raw.githubusercontent.com/LaiYanKai/Misc/main/robot.sh
-chmod +x *.sh
-rm -rf ./opencr_update.tar.bz2
-wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+cd ~/turtlebot3_ws/src/turtlebot3
+rm -r turtlebot3_cartographer turtlebot3_navigation2
+cd ~/turtlebot3_ws/
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
+echo 'export ROS_DOMAIN_ID=$1 #TURTLEBOT3' >> ~/.bashrc
+source ~/.bashrc
+colcon build --symlink-install --parallel-workers 1
+echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
+source ~/.bashrc
 
-# install packages
-sudo apt update
-sudo apt upgrade --yes
+# Some communication protocol for LIDAR
+sudo cp `ros2 pkg prefix turtlebot3_bringup`/share/turtlebot3_bringup/script/99-turtlebot3-cdc.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
-sudo apt install ros-humble-ros-base python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential ros-humble-hls-lfcd-lds-driver ros-humble-turtlebot3-msgs ros-humble-dynamixel-sdk libudev-dev --yes
-
+# For OpenCR
 sudo dpkg --add-architecture armhf
 sudo apt update
-sudo apt install libc6:armhf --yes
+sudo apt install libc6:armhf
+export OPENCR_PORT=/dev/ttyACM0
+export OPENCR_MODEL=burger
 
-reboot
+# Flash OpenCR
+rm -rf ./opencr_update.tar.bz2
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+tar -xvf ./opencr_update.tar.bz2
+cd ~/opencr_update
+./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
