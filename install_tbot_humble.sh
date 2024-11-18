5c5df6d4b4d0549286ee8273fed0f160c8c080d2
Lai Yan Kai <lai.yankai@gmail.com>
2024-11-18 14:09:16 +0800

    Update install_tbot_humble.sh

--- install_tbot_humble.sh
+++ install_tbot_humble.sh
@@ -1,7 +1,6 @@
-# first time username=ubuntu, password=ubuntu
+# first time login username=ubuntu, password=ubuntu
 # set to lab password.
-
-# make sure to connect to a wifi via netplan first.
+set -euxo pipefail
 
 # netplan
 # sudo sh -c "echo 'network:' > /etc/netplan/50-cloud-init.yaml"
@@ -10,13 +9,13 @@
 # sudo sh -c "echo '    eth0:' >> /etc/netplan/50-cloud-init.yaml"
 # sudo sh -c "echo '      dhcp4: true' >> /etc/netplan/50-cloud-init.yaml"
 # sudo sh -c "echo '      optional: true' >> /etc/netplan/50-cloud-init.yaml"
-# sudo sh -c "echo '#  wifis:' >> /etc/netplan/50-cloud-init.yaml"
-# sudo sh -c "echo '#    wlan0:' >> /etc/netplan/50-cloud-init.yaml"
-# sudo sh -c "echo '#      dhcp4: true' >> /etc/netplan/50-cloud-init.yaml"
-# sudo sh -c "echo '#      optional: true' >> /etc/netplan/50-cloud-init.yaml"
-# sudo sh -c "echo '#      access-points: ' >> /etc/netplan/50-cloud-init.yaml"
-# sudo sh -c "echo '#        "<WIFI_SSID>": ' >> /etc/netplan/50-cloud-init.yaml"
-# sudo sh -c "echo '#          password: <WIFI_PASSWORD>' >> /etc/netplan/50-cloud-init.yaml"
+# sudo sh -c "echo '  wifis:' >> /etc/netplan/50-cloud-init.yaml"
+# sudo sh -c "echo '    wlan0:' >> /etc/netplan/50-cloud-init.yaml"
+# sudo sh -c "echo '      dhcp4: true' >> /etc/netplan/50-cloud-init.yaml"
+# sudo sh -c "echo '      optional: true' >> /etc/netplan/50-cloud-init.yaml"
+# sudo sh -c "echo '      access-points: ' >> /etc/netplan/50-cloud-init.yaml"
+# sudo sh -c "echo '        "$1": ' >> /etc/netplan/50-cloud-init.yaml"
+# sudo sh -c "echo '          password: "$2"' >> /etc/netplan/50-cloud-init.yaml"
 # echo 'Run sudo nano /etc/netplan/50-cloud-init.yaml and change the <WIFI_SSID> and <WIFI_PASSWORD>. If password is 1234, replace <WIFI_PASSWORD> with "1234", including quotes. If SSID is mallab16, replace <WIFI_SSID> with "mallab16", including quotes.'
 
 # auto upgrades
@@ -26,10 +25,8 @@ sudo sed -i 's/"1"/"0"/g' /etc/apt/apt.conf.d/20auto-upgrades
 systemctl mask systemd-networkd-wait-online.service
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
-
 # get next script
 wget https://raw.githubusercontent.com/LaiYanKai/Misc/main/install_tbot_humble2.sh
-wget https://raw.githubusercontent.com/LaiYanKai/Misc/main/install_tbot_humble3.sh
 
 chmod +x *.sh
 
@@ -40,7 +37,8 @@ sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 # sudo nano /etc/fstab
-echo 'Append "/swapfile swap swap defaults 0 0" into /etc/fstab using: "sudo nano /etc/fstab"'
-echo 'Then, reboot with "reboot"'
+sudo sh -c "echo '/swapfile swap swap defaults 0 0' >> /etc/fstab"
+# echo 'Append "/swapfile swap swap defaults 0 0" into /etc/fstab using: "sudo nano /etc/fstab"'
+# echo 'Then, reboot with "reboot"'
 
-# reboot
+reboot
