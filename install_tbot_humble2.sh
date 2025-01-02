fa610f92b771404a9c0204c4355f5d3959062545
Lai Yan Kai <lai.yankai@gmail.com>
2025-01-02 16:57:33 +0800

    Update install_tbot_humble2.sh

--- install_tbot_humble2.sh
+++ install_tbot_humble2.sh
@@ -44,6 +44,7 @@ export OPENCR_PORT=/dev/ttyACM0
 export OPENCR_MODEL=burger
 
 # Flash OpenCR
+cd ~
 rm -rf ./opencr_update.tar.bz2
 wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
 tar -xvf ./opencr_update.tar.bz2
