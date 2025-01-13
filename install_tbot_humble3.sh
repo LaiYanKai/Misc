52a088362f3a25c78a99e959768b3ab150e98d88
Lai Yan Kai <lai.yankai@gmail.com>
2025-01-13 15:33:07 +0800

    Update install_tbot_humble3.sh

--- install_tbot_humble3.sh
+++ install_tbot_humble3.sh
@@ -6,6 +6,9 @@ cd ~/rplidar
 colcon build --symlink-install --parallel-workers 2
 source ~/rplidar/install/setup.bash
 
+# rplidar udev rules
+source ~/rplidar/src/rplidar_ros/scripts/create_udev_rules.sh
+
 # build tbot
 cd ~/turtlebot3_ws/src/turtlebot3/turtlebot3_bringup/launch
 wget https://raw.githubusercontent.com/LaiYanKai/Misc/main/robot_c1.launch.py
