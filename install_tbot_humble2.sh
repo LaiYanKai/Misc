6ae141fd5f20f5e7198984279d736f67d7659d24
Lai Yan Kai <lai.yankai@gmail.com>
2025-01-13 14:10:30 +0800

    Update install_tbot_humble2.sh

--- install_tbot_humble2.sh
+++ install_tbot_humble2.sh
@@ -25,7 +25,15 @@ echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
 echo 'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp #for Nav2' >> ~/.bashrc
 source /opt/ros/humble/setup.bash
 
-# build
+# build rplidar
+mkdir -p ~/rplidar/src
+cd ~/rplidar/src
+git clone -b ros2 https://github.com/Slamtec/rplidar_ros.git
+cd ~/rplidar
+colcon build --symlink-install --parallel-workers 2
+source ~/rplidar/install/setup.bash
+
+# build tbot
 cd ~/turtlebot3_ws/src/turtlebot3/turtlebot3_bringup/launch
 wget https://raw.githubusercontent.com/LaiYanKai/Misc/main/robot_c1.launch.py
 cd ~/turtlebot3_ws
