674701663ff59b37c43dde4c9b9c7ac5cf94841a
Lai Yan Kai <lai.yankai@gmail.com>
2025-12-11 18:28:36 +0800

    update flash.sh

--- tb3_jazzy_c1_cam3/flash.sh
+++ tb3_jazzy_c1_cam3/flash.sh
@@ -6,10 +6,12 @@ mkdir -p $HOME/opencr_update
 cd $HOME/opencr_update
 export OPENCR_PORT=/dev/ttyACM0  
 export OPENCR_MODEL=burger
+
 # uncomment the following to update
-# wget -O https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
-# rm -rf opencr_update
-# tar -xvf opencr_update.tar.bz2
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+rm -rf opencr_update
+tar -xvf opencr_update.tar.bz2
+
 cd $HOME/opencr_update/opencr_update  
 ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr  
 cd $HOME
